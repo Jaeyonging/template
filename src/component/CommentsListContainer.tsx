8a8ed08c1dd42f6b5942cec89d5ffd261dd5e837
jaeyonging <wodyd1318@naver.com>
2025-03-12 16:09:55 +0900

    add: zustand and del redux

--- src/component/CommentsListContainer.tsx
+++ src/component/CommentsListContainer.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import useFetchDataStore from '../store/data'
 
 
 const CommentsListContainer = () => {
-
+  const {data} = useFetchDataStore()
   return (
     <div>
-      
+      {data.map((item:any)=>{
+        return <div key={item.id}>{item.name}</div>
+      })}
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,22 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
-import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
-import store from "./store/configureStore";
 import GlobalErrorBoundary from "./boundary/GlobalErrorBoundary";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <GlobalErrorBoundary>
-    <Provider store={store}>
       <BrowserRouter>
         <QueryClientProvider client={queryClient}>
         <App />
         </QueryClientProvider>
       </BrowserRouter>
-    </Provider>
   </GlobalErrorBoundary>
 );
